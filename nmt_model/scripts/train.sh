1a3ce335c4b9727b41f1c08da940fdbfadf84e42
Srividya1729 <spothara@andrew.cmu.edu>
2018-10-22 11:50:29 -0400

    Folder restructuring + nmt code

---
+++ nmt_model/scripts/train.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+vocab="data/vocab.bin"
+train_src="/home/anushap/Code-Generation/data/nl_train.txt"
+train_tgt="/home/anushap/Code-Generation/data/code_train.txt"
+# train_src="data/valid.de-en.de"
+# train_tgt="data/valid.de-en.en"
+dev_src="/home/anushap/Code-Generation/data/nl_dev.txt"
+dev_tgt="/home/anushap/Code-Generation/data/code_dev.txt"
+test_src="/home/anushap/Code-Generation/data/nl_test.txt"
+test_tgt="/home/anushap/Code-Generation/data/code_test.txt"
+
+work_dir="work_dir"
+
+mkdir -p ${work_dir}
+echo save results to ${work_dir}
+
+python nmt.py \
+    train \
+    --cuda \
+    --vocab ${vocab} \
+    --train-src ${train_src} \
+    --train-tgt ${train_tgt} \
+    --dev-src ${dev_src} \
+    --dev-tgt ${dev_tgt} \
+    --save-to ${work_dir}/model.bin \
+    --valid-niter 50 \
+    --batch-size 16 \
+    --hidden-size 256 \
+    --embed-size 128 \
+    --uniform-init 0.1 \
+    --dropout 0.2 \
+    --clip-grad 5.0 \
+    --lr-decay 0.5 2>${work_dir}/err.log
+
+python nmt.py \
+   decode \
+   --cuda \
+   --beam-size 10 \
+   --max-decoding-time-step 20 \
+   ${work_dir}/model.bin \
+   ${test_src} \
+   ${work_dir}/decode.txt
+
+perl multi-bleu.perl ${test_tgt} < ${work_dir}/decode.txt
---
+++ nmt_model/vocab.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+"""
+Usage:
+    vocab.py --train-src=<file> --train-tgt=<file> [options] VOCAB_FILE
+
+Options:
+    -h --help                  Show this screen.
+    --train-src=<file>         File of training source sentences
+    --train-tgt=<file>         File of training target sentences
+    --size=<int>               vocab size [default: 50000]
+    --freq-cutoff=<int>        frequency cutoff [default: 2]
+"""
+
+from typing import List
+from collections import Counter
+from itertools import chain
+from docopt import docopt
+import pickle
+import torch
+import pdb
+
+from utils import read_corpus, input_transpose
+
+
+class VocabEntry(object):
+    def __init__(self):
+        self.word2id = dict()
+        self.unk_id = 3
+        self.word2id['<pad>'] = 0
+        self.word2id['<s>'] = 1
+        self.word2id['</s>'] = 2
+        self.word2id['<unk>'] = 3
+
+        self.id2word = {v: k for k, v in self.word2id.items()}
+
+    def __getitem__(self, word):
+        return self.word2id.get(word, self.unk_id)
+
+    def __contains__(self, word):
+        return word in self.word2id
+
+    def __setitem__(self, key, value):
+        raise ValueError('vocabulary is readonly')
+
+    def __len__(self):
+        return len(self.word2id)
+
+    def __repr__(self):
+        return 'Vocabulary[size=%d]' % len(self)
+
+    def id2word(self, wid):
+        return self.id2word[wid]
+
+    def add(self, word):
+        if word not in self:
+            wid = self.word2id[word] = len(self)
+            self.id2word[wid] = word
+            return wid
+        else:
+            return self[word]
+
+    def words2indices(self, sents):
+        if type(sents[0]) == list:
+            return [[self[w] for w in s] for s in sents]
+        else:
+            return [self[w] for w in sents]
+
+    def indices2words(self, word_ids):
+        return [self.id2word[w_id] for w_id in word_ids]
+
+    def to_input_tensor(self, sents: List[List[str]], device: torch.device) -> torch.Tensor:
+        word_ids = self.words2indices(sents)
+        sents_t = input_transpose(word_ids, self['<pad>'])
+
+        sents_var = torch.tensor(sents_t, dtype=torch.long, device=device)
+
+        return sents_var
+
+    @staticmethod
+    def from_corpus(corpus, size, freq_cutoff=2):
+        vocab_entry = VocabEntry()
+
+        word_freq = Counter(chain(*corpus))
+        valid_words = [w for w, v in word_freq.items() if v >= freq_cutoff]
+        print(f'number of word types: {len(word_freq)}, number of word types w/ frequency >= {freq_cutoff}: {len(valid_words)}')
+
+        top_k_words = sorted(valid_words, key=lambda w: word_freq[w], reverse=True)[:size]
+        for word in top_k_words:
+            vocab_entry.add(word)
+
+        return vocab_entry
+
+
+class Vocab(object):
+    def __init__(self, src_sents, tgt_sents, vocab_size, freq_cutoff):
+        assert len(src_sents) == len(tgt_sents)
+
+        print('initialize source vocabulary ..')
+        self.src = VocabEntry.from_corpus(src_sents, vocab_size, freq_cutoff)
+
+        print('initialize target vocabulary ..')
+        self.tgt = VocabEntry.from_corpus(tgt_sents, vocab_size, freq_cutoff)
+
+    def __repr__(self):
+        return 'Vocab(source %d words, target %d words)' % (len(self.src), len(self.tgt))
+
+
+if __name__ == '__main__':
+    
+    src_sents, src_f_ids = read_corpus('/home/anushap/Code-Generation/data/nl_train.txt', source='src')
+    tgt_sents, tgt_f_ids  = read_corpus('/home/anushap/Code-Generation/data/code_train.txt', source='tgt')
+#     pdb.set_trace()
+
+    total_failed_ids = set(src_f_ids).union(tgt_f_ids)
+    src_sents = [src_sents[i] for i in range(len(src_sents)) if i not in total_failed_ids]
+    tgt_sents = [tgt_sents[i] for i in range(len(tgt_sents)) if i not in total_failed_ids]
+    
+    vocab = Vocab(src_sents, tgt_sents, 7000, freq_cutoff=2)
+    print('generated vocabulary, source %d words, target %d words' % (len(vocab.src), len(vocab.tgt)))
+
+    pickle.dump(vocab, open('data/vocab.bin', 'wb'))
+    print('vocabulary saved to vocab.bin')
+    
+#     args = docopt(__doc__)
+
+#     print('read in source sentences: %s' % args['--train-src'])
+#     print('read in target sentences: %s' % args['--train-tgt'])
+
+#     src_sents = read_corpus(args['--train-src'], source='src')
+#     tgt_sents = read_corpus(args['--train-tgt'], source='tgt')
+
+#     vocab = Vocab(src_sents, tgt_sents, int(args['--size']), int(args['--freq-cutoff']))
+#     print('generated vocabulary, source %d words, target %d words' % (len(vocab.src), len(vocab.tgt)))
+
+#     pickle.dump(vocab, open(args['VOCAB_FILE'], 'wb'))
+#     print('vocabulary saved to %s' % args['VOCAB_FILE'])
